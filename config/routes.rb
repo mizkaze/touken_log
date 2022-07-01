bc9b43cddd7cd7c30c7ff1034eab29a782675a4d
mizkaze <watashi03go@gmail.com>
2022-07-01 14:41:05 +0000

    [Finish]users sign_up(add name)

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'homes/top'
-  end
-  # adminは登録画面とパス変更画面はいらないから↓
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-
   devise_for :users, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+
+
+  root to: "homes#top"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_075650) do
+ActiveRecord::Schema.define(version: 2022_07_01_143002) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,18 +40,6 @@ ActiveRecord::Schema.define(version: 2022_06_29_075650) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,9 +49,11 @@ ActiveRecord::Schema.define(version: 2022_06_29_075650) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.text "introduction", null: false
+    t.text "introduction"
     t.integer "status", default: 0, null: false
     t.boolean "is_active", default: true, null: false
+    t.boolean "admin", default: false, null: false
+    t.integer "kind", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
