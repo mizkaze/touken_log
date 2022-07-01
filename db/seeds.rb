aff0300b53dc782adce88e8e5435c5a0c29b6b77
mizkaze <watashi03go@gmail.com>
2022-07-01 18:15:16 +0000

    Save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "管理人",
+             email: "admin@example.com",
+             password:  "test.01",
+             password_confirmation: "test.01",
+             admin: true)
+
